e5c7b370fdce8e0a49d5ba6b865b6461f16ca80a
shanley2 <shanley907@gmail.com>
2024-02-16 11:22:07 -0500

    added extra steps

--- SentimentAnalysis/sentiment_analysis.py
+++ SentimentAnalysis/sentiment_analysis.py
@@ -1,11 +1,17 @@
-import requests
+"""
+Module inside of Sentiment Analysis package which analyzes text
+for positive or negative or neutral feelings
+"""
 import json
+import requests
 
 def sentiment_analyzer(text_to_analyse):
-    url = 'https://sn-watson-sentiment-bert.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict'
+    """Function which takes in text and uses the Watson Sentiment
+    Analyzer to analyze given input for positive or negative feelings"""
+    url = '''https://sn-watson-sentiment-bert.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict'''
     header =  {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}
     myobj  = {"raw_document": { "text": text_to_analyse } }
-    response = requests.post(url, json = myobj, headers=header)
+    response = requests.post(url, json = myobj, headers=header, timeout=10)
     formatted_response = json.loads(response.text)
     if response.status_code == 200:
         label = formatted_response['documentSentiment']['label']
@@ -14,4 +20,3 @@ def sentiment_analyzer(text_to_analyse):
         label = None
         score = None
     return {"label": label, "score": score}
-
